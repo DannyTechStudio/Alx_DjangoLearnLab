37b66800a2ed8251df8fd80d92bead665b6aac64
Daniel Offei <archimedes301@gmail.com>
2025-11-07 20:05:59 +0000

    Implement user authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 # Create your views here.
 # Function-based views
@@ -15,3 +19,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# Login view (built-in)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view (built-in)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
