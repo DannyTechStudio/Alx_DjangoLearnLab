9b56992d05b8e50788bac8710105d5a052991575
Daniel Offei <archimedes301@gmail.com>
2025-11-07 19:04:51 +0000

    Modify views.py and urls.py files to meet the checker's requirements

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.get_book_lists, name='book_list'),
-    path('library/<int:pk>/', views.get_library_details.as_view(), name='library_detail'),
+    path('books/', list_books, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,13 +5,13 @@ from .models import Library
 
 # Create your views here.
 # Function-based views
-def get_book_lists(request):
+def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
 # class based-views
-class get_library_details(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
