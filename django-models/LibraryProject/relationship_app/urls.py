580e35b7651f236708f0ef5737ba95b612b62559
Daniel Offei <archimedes301@gmail.com>
2025-11-08 23:18:21 +0000

    Update URL patterns to match expected add_book and edit_book paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('member_view/', views.member_view, name='member_view'),
     
     # Permissions URLs
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
