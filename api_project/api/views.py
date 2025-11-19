24799005dee253845a93a3d5d6908de4958fab14
Daniel Offei <archimedes301@gmail.com>
2025-11-19 18:24:41 +0000

    Implement CRUD Operations with ViewSets and Routers in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book 
 
@@ -9,4 +10,11 @@ class BookList(generics.ListAPIView):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+      
+        
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
     
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)       # Automatically set the author to the logged-in user
