4cb2d14289858fb90aeb4dee667db3c63ce862fa
Daniel Offei <archimedes301@gmail.com>
2025-11-29 22:46:46 +0000

    Ensure the application of Django REST Frameworks permission classes to protect API endpoints based on user roles

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from .models import Book
 from .serializers import BookSerializer
 
