b90f4d0983fdb673cc4d9321c6c1d91c961e0ac8
Daniel Offei <archimedes301@gmail.com>
2025-12-04 07:31:32 +0000

    Update the urls patterns for the CRUD operations.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('register/', register, name='register'),       #--------- Registration (custom)
     path('profile/', profile, name='profile'),          #--------- Profile page
     path('', PostListView.as_view(), name='post-list'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('new/', PostCreateView.as_view(), name='post-create'),
-    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
